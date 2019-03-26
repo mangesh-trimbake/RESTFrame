241701070f0a330e646c3769ac79e1bd1418c7e3
mangesh-trimbake <mangeshtrimbake00786.94@gmail.com>
2019-03-26 12:17:31 +0530

    handles_class.php file created, which has all handlers associated class with get,post method

---
+++ handlers_class.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class indexHandler{
+
+	function get(){
+		# code...
+		echo "Hello Wolrd, this is root of project via get()";
+	}
+
+	function post(){
+		# code...
+		echo "Hello Wolrd, this is root of project via post()";
+	}
+}
+
+
+class userAdd{
+
+	function get(){
+
+		echo "userAdd class, get()";
+
+	}
+
+	function post(){
+
+		echo "userAdd class, post()";
+	}
+	
+}
+
+
+?>
+
